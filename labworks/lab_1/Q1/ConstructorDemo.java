5c7bd2414138ea3728bf2e371e1c058b1c7e60a8
Suraj-Pahari <surajpahari152@gmail.com>
2021-12-09 15:14:50 +0545

    Q1(consturctor demonstration) completed

--- labworks/lab_1/Q1/ConstructorDemo.java
+++ labworks/lab_1/Q1/ConstructorDemo.java
@@ -3,20 +3,20 @@ class ConstructorDemo{
     int value;
 
     ConstructorDemo(){
-         System.out.println("This is the default constructor"); 
+         System.out.println("This is the default constructor."); 
     }
 
     ConstructorDemo(int value){
-        System.out.println("This is the parameterized contructor");
+        System.out.println("This is the parameterized contructor.");
 
         this.value = value;
-        System.out.println("The value is set to "+this.value);
+        System.out.println("The value is set to "+this.value+".");
 
     }
 
     ConstructorDemo(ConstructorDemo c1){
         c1 = new ConstructorDemo();
-        System.out.println("This is the copy constructor");
+        System.out.println("This is the copy constructor.");
     }
 
     public static void main (String[] args){
