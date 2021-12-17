d8af46b8112eafb08859d9e6ade778e3c8f0f2dd
Suraj-Pahari <surajpahari152@gmail.com>
2021-12-17 20:50:47 +0545

    Q4(demo of multiple inheritance completed)

--- labworks/lab_1/Q4/Omnivore.java
+++ labworks/lab_1/Q4/Omnivore.java
@@ -17,12 +17,16 @@ interface Carnivore{
 class Omnivore implements Herbivore ,Carnivore{
 
     public void print(){
+
         Herbivore.super.print();
         Carnivore.super.print();
+
     }
     
     public static void main(String [] args){
+
         Omnivore o1 = new Omnivore();
         o1.print();
+        
     }
 }
