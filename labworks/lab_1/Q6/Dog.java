37fc8027291d813f053f292dacd3432368c464ad
Suraj-Pahari <surajpahari152@gmail.com>
2021-12-17 20:51:14 +0545

    Q6(demo of super and final keyword) completed

--- labworks/lab_1/Q6/Dog.java
+++ labworks/lab_1/Q6/Dog.java
@@ -2,13 +2,14 @@ class Dog extends Animal
 {
     String type ;
     Dog (String type){
-        super();
+        super(); //calls the constructor of parent i.e of class Animal;
         this.type= type;
     }
+
     void classify(){
-        // super();
         System.out.println("type:"+this.type);
     }
+
     public static void main(String [] args){
 
         Dog d1 = new Dog("labrador");
