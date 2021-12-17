9a734e794f80d90457466a75cf0f64b8922ffc8d
Suraj-Pahari <surajpahari152@gmail.com>
2021-12-17 09:52:06 +0545

    Q4(demo of multiple inheritance completed)

---
+++ labworks/lab_1/Q4/Omnivore.java
@@ -0,0 +1,27 @@
+interface Herbivore{
+
+    default void print(){
+        System.out.println("Eats plant.");
+    }
+
+}
+
+interface Carnivore{
+
+    default void print(){
+        System.out.println("Eats animal.");
+    }
+
+}
+
+class Omnivore implements Herbivore ,Carnivore{
+
+    public void print(){
+        Herbivore.super.print();
+        Carnivore.super.print();
+    }
+    public static void main(String [] args){
+        Omnivore o1 = new Omnivore();
+        o1.print();
+    }
+}
